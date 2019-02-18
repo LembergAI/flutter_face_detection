370207f50c9e9287e32036a6450d6ec40a1d7b5f
Arto Bendiken <arto@lemberg.ai>
2019-02-18 10:13:58 +0200

    Fixed style nits with `flutter format`.

--- lib/src/face_painter.dart
+++ lib/src/face_painter.dart
@@ -22,19 +22,19 @@ class FacePainter extends CustomPainter {
   @override
   void paint(final Canvas canvas, final Size size) {
     final line = Paint()
-        ..color = color
-        ..style = PaintingStyle.stroke
-        ..strokeCap = StrokeCap.round
-        ..strokeWidth = strokeWidth;
+      ..color = color
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = strokeWidth;
 
     for (final face in faces) {
       final midPoint = Offset(face.midPoint.x, face.midPoint.y);
-      final leftEye = midPoint.translate(-(face.eyesDistance/2), 0);
-      final rightEye = midPoint.translate((face.eyesDistance/2), 0);
+      final leftEye = midPoint.translate(-(face.eyesDistance / 2), 0);
+      final rightEye = midPoint.translate((face.eyesDistance / 2), 0);
       final noseBridge = Path()
-        ..moveTo(leftEye.dx+eyeRadius, leftEye.dy)
-        ..quadraticBezierTo(midPoint.dx, midPoint.dy-eyeRadius,
-            rightEye.dx-eyeRadius, rightEye.dy);
+        ..moveTo(leftEye.dx + eyeRadius, leftEye.dy)
+        ..quadraticBezierTo(midPoint.dx, midPoint.dy - eyeRadius,
+            rightEye.dx - eyeRadius, rightEye.dy);
       canvas.drawCircle(leftEye, eyeRadius, line);
       canvas.drawCircle(rightEye, eyeRadius, line);
       canvas.drawPath(noseBridge, line);
